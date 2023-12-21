238f0912a7657e041483b6d216610df17c93249c
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-21 03:54:28 +0100

    back to tasks

--- models/state.py
+++ models/state.py
@@ -1,31 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-
-    __tablename__ = 'states'
-    if storage_type == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade="all,delete", backref="state")
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """getter docuemnt"""
-            from models import storage
-            citiesList = []
-            citiesAll = storage.all(City)
-            for city in citiesAll.values():
-                if city.state_id == self.id:
-                    citiesList.append(city)
-            return citiesList
+    name = ""
