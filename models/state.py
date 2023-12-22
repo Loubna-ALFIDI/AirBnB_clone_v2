560e6d8c3d6ca0f3fd9ab4ec0be2b0f697a00d49
Loubna <loubna.alfidi@gmail.com>
2023-12-22 01:42:50 +0100

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -4,13 +4,19 @@ from models.base_model import BaseModel, Base
 from models.city import City
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state', cascade="all,delete")
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade="all,delete")
+    else:
+        name = ""
 
     @property
     def cities(self):
