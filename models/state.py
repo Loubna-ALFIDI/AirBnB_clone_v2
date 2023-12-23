2eff2199cca8b20080ede243127c4397d6f7df48
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-23 13:12:12 +0100

    fix state

--- models/state.py
+++ models/state.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
-from os import getenv
-
-storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade="all,delete")
-    else:
-        name = ""
+    name = ""
+#from models.city import City
+#from sqlalchemy import Column, String, ForeignKey
+#from sqlalchemy.orm import relationship
+#from os import getenv
 
-    @property
-    def cities(self):
-        """ Getter attribute to retrieve a list of cities"""
-        from models import storage
+#storage_type = getenv("HBNB_TYPE_STORAGE")
 
-        city_list = []
-        for city_id, city_list in storage.all(City).items():
-            if city_list.state_id == self.id:
-                city_list.append(city_list)
-        return city_list
+
+#class State(BaseModel, Base):
+#    """ State class """
+#    __tablename__ = 'states'
+#    if storage_type == 'db':
+#        name = Column(String(128), nullable=False)
+#        cities = relationship('City', backref='state', cascade="all,delete")
+#    else:
+#        name = ""
+
+#    @property
+#    def cities(self):
+#        """ Getter attribute to retrieve a list of cities"""
+#        from models import storage
+
+#        city_list = []
+#        for city_id, city_list in storage.all(City).items():
+#            if city_list.state_id == self.id:
+#                city_list.append(city_list)
+#        return city_list
