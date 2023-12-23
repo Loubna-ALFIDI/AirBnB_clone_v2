52f1c7c61096a11623dbf47fbbcf26de9ace365c
HichamLuffy <imalxvssteve2020@gmail.com>
2023-12-23 13:00:00 +0100

    fix line 66 - 69 file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -63,3 +63,7 @@ class FileStorage:
             if key in self.__objects:
                 del self.__objects[key]
                 self.save()
+            else:
+                pass
+        else:
+            pass
