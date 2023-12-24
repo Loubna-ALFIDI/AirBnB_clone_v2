f5044b491a17007fe634e1c19f9837b4ee90fccd
Loubna <loubna.alfidi@gmail.com>
2023-12-24 01:38:14 +0100

    Updating User

--- models/user.py
+++ models/user.py
@@ -14,10 +14,10 @@ class User(BaseModel, Base):
     if storage_type == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship('Place', cascade="all,delete", backref="user")
-        reviews = relationship('Review', cascade="all,delete", backref="user")
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship('Place', cascade="all,delete-orphan", backref="user")
+        reviews = relationship('Review', cascade="all,delete-orphan", backref="user")
     else:
         email = ""
         password = ""
