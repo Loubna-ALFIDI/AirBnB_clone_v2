4a0eef6a87a2305850f181f09fc0b4104a77051f
Loubna <loubna.alfidi@gmail.com>
2024-01-11 20:11:50 +0100

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -22,7 +22,7 @@ echo "$index_content" | sudo tee "/data/web_static/releases/test/index.html" > /
 rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo chown -hR ubuntu:ubuntu "/data/"
+sudo chown -hR loubna:loubna "/data/"
 
 sudo wget -q -O /etc/nginx/sites-available/default http://exampleconfig.com/static/raw/nginx/ubuntu20.04/etc/nginx/sites-available/default
 config="/etc/nginx/sites-available/default"
