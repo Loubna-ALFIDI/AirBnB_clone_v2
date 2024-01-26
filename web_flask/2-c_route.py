3cd196e7bf11d6c4e70a0cdf718d82fb9e79e076
Loubna <loubna.alfidi@gmail.com>
2024-01-26 20:39:13 +0100

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" module Hello Flask! """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """ def hellowww """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ def hbnb """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """ def c """
+    return "C {}".format(text.replace("_", " "))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
